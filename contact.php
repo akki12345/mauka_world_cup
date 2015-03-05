d61568718a427ffc0f04fe38f34c3703050db1c5
ashish-shrma <ashish851995@gmail.com>
2015-03-06 02:18:37 +0530

    conatcts and about

---
+++ contact.php
@@ -0,0 +1,131 @@
+<?php
+require 'header.php';
+?>
+<!--  Start Secondary Navbar   -->
+
+<nav class="navbar navbar-inverse hidden-sm hidden-xs sec-navbar">
+<div class="container">
+  <div id="navbar" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">All</a></li>
+       <li><a href="#">ICC World Cup</a></li>
+      <li><a href="#">UEFA</a></li>
+      <li><a href="#">La Liga</a></li>
+      <li><a href="single.html">Talk To Star Players</a></li>
+      <li><a href="#">Blogs/News</a></li>
+    </ul>
+  </div>
+</div>
+</nav>
+<!-- ./ Secondary Navbar  -->
+
+<!--  Start Main Content (left section + right sidebar)   -->
+
+<section class="container">
+  <div class="row">
+
+<!--  Start Left Section  col-md-9  -->
+
+    <div class="col-md-9">
+      <div class="main"><!-- start main -->
+<div class="container">
+			<div class="row contact"><!-- start contact -->				
+				<div class="col-md-3">
+					<div class="contact_info">
+			    	 	<h2>Find Us Here</h2>
+			    	 		<div class="map">
+			    	 		<iframe width="100%" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3499.95412049006!2d77.214592!3d28.691019!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd94c8b62999%3A0xae0071f78605288f!2sDelhi+University+Stadium%2C+University+of+Delhi+North+Campus!5e0!3m2!1sen!2sin!4v1425585011963" width="600" height="450" frameborder="0" style="border:0"></iframe>
+					   		</div>
+      				</div>
+      			<div class="company_ad">
+				     	<h2>Find Address :</h2>
+      				<address>
+						 <p>500 Lorem Ipsum Dolor Sit,</p>
+						 <p>22-56-2-9 Sit Amet, Lorem,</p>
+						 <p>USA</p>
+				   		<p>Phone:(00) 222 666 444</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <a href="mailto:info@mycompany.com">info(at)mycompany.com</a></p>
+				   		<p>Follow on: <a href="#">Facebook</a>, <a href="#">Twitter</a></p>
+				   	</address>
+				   </div>
+				</div>				
+				<div class="col-md-6">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form method="post" action="contact-post.html">
+					    	<div>
+						    	<span>Name</span>
+						    	<span><input type="username" class="form-control" id="userName"></span>
+						    </div>
+						    <div>
+						    	<span>E-Mail</span>
+						    	<span><input type="email" class="form-control" id="inputEmail3"></span>
+						    </div>
+						    <div>
+						     	<span>Mobile</span>
+						    	<span><input type="number" class="form-control" id="number"></span>
+						    </div>
+						    <div>
+						    	<span>Subject</span>
+						    	<span><textarea class="form-control" name="userMsg"> </textarea></span>
+						    </div>
+						    <br>
+						   <div>
+						   		<span><input type="submit" value="submit" class="btn btn-success"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>		
+  				<div class="clearfix"></div>		
+		  </div> <!-- end contact -->
+</div>
+</div>
+    </div>
+
+<!--  ./ Left Section  col-md-9  -->
+
+<!--  Start Right Sidebar  col-md-3  -->
+
+    <div class="col-md-3 main-sidebar">
+        <div id="rsidebar-wrapper">
+<div class="widget HTML" id="HTML1">
+<h3 class="title">Featured Vidoes</h3>
+<div class="widget-content">
+
+<script src="./5 True Facts about Android!-Videoism_files/Feature"></script><ul class="recent_posts_with_thumbs"><li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.html" target="_top"><iframe width="200" height="180" src="http://www.youtube.com/embed/C3KuJKNFFjU">
+</iframe></a></div><b><a href="video.html" target="_top">Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.html" target="_top"><iframe width="200" height="180" src="http://www.youtube.com/embed/gtSSxgBayyk">
+</iframe></a></div><b><a href="video.html" target="_top">Mauka Mauka Counter Strike Edition</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.html" target="_top"><iframe width="200" height="180" src="http://www.youtube.com/embed/2MYj3c1Wd6I">
+</iframe></a></div><b><a href="video.html" target="_top">3 players injured in 1 ball - Rare Epic Cricket Moment</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.html" target="_top"><iframe width="200" height="180" src="http://www.youtube.com/embed/pijivK5zfQA">
+</iframe></a></div><b><a href="video.html" target="_top">Baap Baap Hota hai Beta Beta</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.html" target="_top"><iframe width="200" height="180" src="http://www.youtube.com/embed/7VDN-yPr9as">
+</iframe></a></div><b><a href="video.html" target="_top">Shoaib Akhtar Kill Pakistani Team</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+
+</div>
+<div class="clear"></div>
+
+<div class="clear"></div>
+</div></div>
+<p></p></div>
+      </ul>
+      <div class="">
+        <a class=" btn-link btn-sm pull-right" href="video.html">View More Videos</a>
+      </div>
+    </div>
+  </div>
+
+  <!--  ./ Right Sidebar  col-md-3  -->
+
+
+
+</section>
+
+<!--  ./ Main Content (left section + right sidebar)   -->
+
+
+<?php
+require 'footer.php';
+?>
