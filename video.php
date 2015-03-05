c01c67f7901cb0b0e63cda9b4a37253397cc5ecd
akki12345 <akoli0082@gmail.com>
2015-03-06 04:16:23 +0530

    youtube link

--- video.php
+++ video.php
@@ -56,16 +56,16 @@
 <iframe width="660" height="350" src="http://www.youtube.com/embed/C3KuJKNFFjU">
 </iframe>
 <h2 class="post-title entry-title">
-<a href="./5 True Facts about Android!-Videoism_files/5 True Facts about Android!-Videoism.html">5 True Facts about Android!</a>
+<a href="video.php">Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</a>
 </h2>
 <div id="metawrp-main">
 <div class="metawrp">
 <div class="yt-thumb-clip-inner">
-<a href="https://plus.google.com/106374439082237286396"><img alt="My Photo" class="profile-img" height="80" src="./5 True Facts about Android!-Videoism_files/photo.jpg" width="80"></a>
+<a href="https://plus.google.com/106374439082237286396"></a>
 </div>
 <div class="postedby">
 <span class="author"><a href="#">
-Syed Faizan Ali</a></span>
+MaukaMaukaOfficial</a></span>
 </div>
 <br>
 <div id="___follow_0" style="text-indent: 0px; margin: 0px; padding: 0px; background-color: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 131px; height: 24px; background-position: initial initial; background-repeat: initial initial;"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 131px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 24px;" tabindex="0" vspace="0" width="100%" id="I0_1425406872415" name="I0_1425406872415" src="./5 True Facts about Android!-Videoism_files/follow.html" data-gapiattached="true"></iframe></div>
@@ -76,8 +76,8 @@ Syed Faizan Ali</a></span>
 <br><br>
 <div class="labelwrap">
 <strong>Category:</strong>
-<a href="http://videoismtheme.blogspot.in/search/label/Android?&max-results=10" rel="tag">Android</a>,
-<a href="http://videoismtheme.blogspot.in/search/label/Feature?&max-results=10" rel="tag">Feature</a>
+<a href="#" rel="tag">#MaukaMauka</a>,
+<a href="#" rel="tag">Sikh Boy</a>
 </div>
 </div>
 </div>
@@ -93,10 +93,10 @@ Syed Faizan Ali</a></span>
 </div>
 <div class="blog-pager" id="blog-pager">
 <span id="blog-pager-newer-link">
-<a class="blog-pager-newer-link" href="http://videoismtheme.blogspot.in/2013/07/heres-why-you-should-root-your-android.html" id="Blog1_blog-pager-newer-link" title="Newer Post">Newer Post</a>
+<a class="blog-pager-newer-link" href="#" id="Blog1_blog-pager-newer-link" title="Newer Post">Newer Post</a>
 </span>
 <span id="blog-pager-older-link">
-<a class="blog-pager-older-link" href="http://videoismtheme.blogspot.in/2013/07/despicable-me-2-minions-banana-song-2013.html" id="Blog1_blog-pager-older-link" title="Older Post">Older Videos</a>
+<a class="blog-pager-older-link" href="#" id="Blog1_blog-pager-older-link" title="Older Post">Older Videos</a>
 </span>
 </div>
 <div class="clear"></div>
@@ -116,12 +116,6 @@ Syed Faizan Ali</a></span>
       
 <!-- google_ad_section_end -->
 </div>
-<div class="post-feeds">
-<div class="feed-links">
-Subscribe to:
-<a class="feed-link" href="http://videoismtheme.blogspot.com/feeds/2838817585187719172/comments/default" target="_blank" type="application/atom+xml">Post Comments (Atom)</a>
-</div>
-</div>
 <script type="text/javascript">window.___gcfg = {'lang': 'en_GB'};</script>
 </div></div>
 </div>
@@ -139,16 +133,16 @@ var showpostdate = true;
 var showpostsummary = true;
 var numchars = 75;
 </script>
-<script src="./5 True Facts about Android!-Videoism_files/Feature"></script><ul class="recent_posts_with_thumbs"><li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.php" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/C3KuJKNFFjU">
-</iframe></a></div><b><a href="video.php" target="_top">Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.php" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/gtSSxgBayyk">
-</iframe></a></div><b><a href="video.php" target="_top">Mauka Mauka Counter Strike Edition</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.php" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/2MYj3c1Wd6I">
-</iframe></a></div><b><a href="video.php" target="_top">3 players injured in 1 ball - Rare Epic Cricket Moment</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.php" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/pijivK5zfQA">
-</iframe></a></div><b><a href="video.php" target="_top">Baap Baap Hota hai Beta Beta</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="video.php" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/7VDN-yPr9as">
-</iframe></a></div><b><a href="video.php" target="_top">Shoaib Akhtar Kill Pakistani Team</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<script src="./5 True Facts about Android!-Videoism_files/Feature"></script><ul class="recent_posts_with_thumbs"><li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=C3KuJKNFFjU" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/C3KuJKNFFjU">
+</iframe></a></div><b><a href="https://www.youtube.com/watch?v=C3KuJKNFFjU" target="_top">Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=gtSSxgBayyk" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/gtSSxgBayyk">
+</iframe></a></div><b><a href="https://www.youtube.com/watch?v=gtSSxgBayyk" target="_top">Mauka Mauka Counter Strike Edition</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=2MYj3c1Wd6I" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/2MYj3c1Wd6I">
+</iframe></a></div><b><a href="https://www.youtube.com/watch?v=2MYj3c1Wd6I" target="_top">3 players injured in 1 ball - Rare Epic Cricket Moment</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=pijivK5zfQA" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/pijivK5zfQA">
+</iframe></a></div><b><a href="https://www.youtube.com/watch?v=pijivK5zfQA" target="_top">Baap Baap Hota hai Beta Beta</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
+<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=7VDN-yPr9as" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/7VDN-yPr9as">
+</iframe></a></div><b><a href="https://www.youtube.com/watch?v=7VDN-yPr9as" target="_top">Shoaib Akhtar Kill Pakistani Team</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
 
 </div>
 <div class="clear"></div>
