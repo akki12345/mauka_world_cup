b5ac98be54e56dab444cc0107d3e678ab71fc88e
akki12345 <akoli0082@gmail.com>
2015-03-06 15:53:25 +0530

    new video portal

--- video.php
+++ video.php
@@ -1,161 +1,96 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!-- saved from url=(0072)http://videoismtheme.blogspot.in/2013/07/5-true-facts-about-android.html -->
-<html>
-<head>
-<script id="facebook-jssdk" src="./5 True Facts about Android!-Videoism_files/all.js"></script><script src="./5 True Facts about Android!-Videoism_files/jquery.min.js" type="text/javascript"></script>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/theme.css" rel="stylesheet">
-<title>Mauka Mauka Official</title>
-<link type="text/css" rel="stylesheet" href="./5 True Facts about Android!-Videoism_files/728935430-widget_css_bundle.css">
-<link id="page-skin-1" type="text/css" rel="stylesheet" href="style.css">
-</head>
-<body style="">
+<?php
+require 'header.php';
+?>
+<!--  Start Secondary Navbar   -->
 
-<!-- Start Main-Navbar  -->
+<nav class="navbar navbar-inverse hidden-sm hidden-xs sec-navbar">
 <div class="container">
-  <form class="navbar-form ts-search-field navbar-left" role="search">
-    <div class="form-group">
-      <input type="text" class="form-control ts-search" placeholder="Search for Mauka...">
-    </div>
-  </form>
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <div class="navbar-brand navbar-brand-centered">
-      
-   <a href="#"> <img src="./cup/logo.png" alt="logo"></a>
-    </div>
-  </div>
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="navbar-brand-centered">
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="about.php">About Us</a></li>
-      <li><a href="blog.php">Blogs</a></li>
+  <div id="navbar" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">All</a></li>
+       <li><a href="#" data-tooltip="coming soon">ICC World Cup</a></li>
+      <li><a href="#" data-tooltip="coming soon">UEFA</a></li>
+      <li><a href="#" data-tooltip="coming soon">La Liga</a></li>
+      <li><a href="talk.php">Talk To Star Players</a></li>
+      <li><a href="blog.php">Blogs/News</a></li>
     </ul>
   </div>
-  <!-- /.navbar-collapse -->
 </div>
-<!-- /.container-fluid -->
 </nav>
-<div id="wrapper"><br><br>
-<div id="container-main">
-<div id="main-wrapper">
-<div class="main section" id="main"><div class="widget Blog" id="Blog1">
-<div class="blog-posts hfeed">
-<!-- google_ad_section_start(name=default) -->
-
-          <div class="date-outer">
-
-          <div class="date-posts">
-        
-<div class="post-outer">
-<div class="wrapfullpost">
-<div class="post hentry">
-<a name="2838817585187719172"></a>
-<div class="post-body entry-content">
-<iframe width="660" height="350" src="http://www.youtube.com/embed/eFS1ikcvLdI">
-</iframe>
-<h2 class="post-title entry-title">
-<a href="video.php">Mauka Mauka – INDIA Vs West INDIES #WontGiveItBack</a>
-</h2>
-<div id="metawrp-main">
-<div class="metawrp">
-<div class="yt-thumb-clip-inner">
-<a href="https://plus.google.com/106374439082237286396"></a>
-</div>
-<div class="postedby">
-<span class="author"><a href="#">
-MaukaMaukaOfficial</a></span>
-</div>
-<br>
-<div id="___follow_0" style="text-indent: 0px; margin: 0px; padding: 0px; background-color: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 131px; height: 24px; background-position: initial initial; background-repeat: initial initial;"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 131px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 24px;" tabindex="0" vspace="0" width="100%" id="I0_1425406872415" name="I0_1425406872415" src="./5 True Facts about Android!-Videoism_files/follow.html" data-gapiattached="true"></iframe></div>
-</div>
-<div class="metawrp2">
-<div id="views-container"><span class="views-icon"></span><div class="views-text">Views:</div>
-<div class="viewscount" id="postviews">967</div></div>
-<br><br>
-<div class="labelwrap">
-<strong>Category:</strong>
-<a href="#" rel="tag">#MaukaMauka</a>,
-<a href="#" rel="tag">Sikh Boy</a>
-</div>
-</div>
-</div>
-<div class="postliner"></div>
-<div style="clear: both;"></div>
-</div>
-<div class="post-footer">
-<div class="post-footer-line post-footer-line-"></div>
-<div class="post-footer-line post-footer-line-2"></div>
-<div class="post-footer-line post-footer-line-3">
-</div></div>
-</div>
-</div>
-<div class="blog-pager" id="blog-pager">
-<span id="blog-pager-newer-link">
-<a class="blog-pager-newer-link" href="#" id="Blog1_blog-pager-newer-link" title="Newer Post">Newer Post</a>
-</span>
-<span id="blog-pager-older-link">
-<a class="blog-pager-older-link" href="#" id="Blog1_blog-pager-older-link" title="Older Post">Older Videos</a>
-</span>
-</div>
 <div class="clear"></div>
-<div class="comments" id="comments">
-<a name="comments"></a>
-
-</div>
-<p></p>
-<div id="backlinks-container">
-<div id="Blog1_backlinks-container">
-</div>
-</div>
-</div>
-</div>
-
-        </div></div>
-      
-<!-- google_ad_section_end -->
-</div>
-<script type="text/javascript">window.___gcfg = {'lang': 'en_GB'};</script>
-</div></div>
-</div>
-<div id="rsidebar-wrapper">
-<div class="widget HTML" id="HTML1">
-<h2 class="title">Featured Vidoes</h2>
-<div class="widget-content">
-<script style="text/javascript">
-var numposts = 5;
-var showpostthumbnails = true;
-var displaymore = false;
-var displayseparator = false;
-var showcommentnum = false;
-var showpostdate = true;
-var showpostsummary = true;
-var numchars = 75;
-</script>
-<script src="./5 True Facts about Android!-Videoism_files/Feature"></script><ul class="recent_posts_with_thumbs"><li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=C3KuJKNFFjU" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/C3KuJKNFFjU">
-</iframe></a></div><b><a href="https://www.youtube.com/watch?v=C3KuJKNFFjU" target="_top">Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=gtSSxgBayyk" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/gtSSxgBayyk">
-</iframe></a></div><b><a href="https://www.youtube.com/watch?v=gtSSxgBayyk" target="_top">Mauka Mauka Counter Strike Edition</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=2MYj3c1Wd6I" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/2MYj3c1Wd6I">
-</iframe></a></div><b><a href="https://www.youtube.com/watch?v=2MYj3c1Wd6I" target="_top">3 players injured in 1 ball - Rare Epic Cricket Moment</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=pijivK5zfQA" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/pijivK5zfQA">
-</iframe></a></div><b><a href="https://www.youtube.com/watch?v=pijivK5zfQA" target="_top">Baap Baap Hota hai Beta Beta</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-<li class="clearfix"><div class="zitem"><span class="play-button"></span><a href="https://www.youtube.com/watch?v=7VDN-yPr9as" target="_top"><iframe width="120" height="90" src="http://www.youtube.com/embed/7VDN-yPr9as">
-</iframe></a></div><b><a href="https://www.youtube.com/watch?v=7VDN-yPr9as" target="_top">Shoaib Akhtar Kill Pakistani Team</a></b><br><div style="float:left;padding-top: 5px;"></div></li>
-
-</div>
-<div class="clear"></div>
-<div class="clear"></div>
-</div></div>
-<p></p></div>
-<!-- spacer for skins that want sidebar and main to be the same height-->
-<div class="clear"></div>
-</div>
-<div class="clear"></div>
-</div>
-
-
-<?php
-require 'footer.php';
-?>
+<div class="wrap">
+    <div class="content">
+        <div class="vdo-log margin_bottom">
+            <div class="heading6"><h1>MaukaMaukaOfficial Videos</h1></div>
+            <div class="video-player"><iframe width="600" height="350" src="http://www.youtube.com/embed/eFS1ikcvLdI">
+               </iframe></div>
+            <div class="float-left">
+                <div class="heading5">Description</div>
+                <div class="clear"></div>
+                <div class="vb-desc">Mauka Mauka – INDIA Vs West INDIES #WontGiveItBack.</div>
+                <div class="clear"></div>
+            </div>
+            <div class="float-left">
+                <div class="heading5">Category</div>
+                <div class="clear"></div>
+                <div class="vb-desc"><a href="index.php" rel="nofollow">ICC world cup</a> <span>|</span> <a href="index.php" rel="nofollow">Mauka Mauka</a> </div>
+            </div>
+        <div class="clear"></div>
+        </div>
+    </div>
+    <div class="float-left">
+        <div class="sidebox-one">
+        <div class="heading2"><span>Featured <span class="highlight">Videos</span></span><div class="lines"></div></div>
+            <div class="sb-video-log">
+                <div class="sb-vdo-list">
+                    <div class="sb-vdo-thumb">
+                        <iframe width="250" height="170" src="http://www.youtube.com/embed/C3KuJKNFFjU"></iframe></a>
+                    </div>
+                    <div class="sb-vdo-info">
+                        <div class="sb-vdo-title"><a href="https://www.youtube.com/watch?v=C3KuJKNFFjU"><h4>Sikh Boy Bullied By Racist Kids Calling Him A Terrorist</h4></a></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+              <div class="sb-vdo-list">
+                    <div class="sb-vdo-thumb">
+                       <iframe width="250" height="170" src="http://www.youtube.com/embed/2MYj3c1Wd6I"></iframe></a>
+                    </div>
+                    <div class="sb-vdo-info">
+                        <div class="sb-vdo-title"><a href="https://www.youtube.com/watch?v=2MYj3c1Wd6I"><h4>3 players injured in 1 ball - Rare Epic Cricket Moment</h4></a></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+               <div class="sb-vdo-list">
+                    <div class="sb-vdo-thumb">
+                       <iframe width="250" height="170" src="http://www.youtube.com/embed/pijivK5zfQA"></iframe></a>
+                    </div>
+                    <div class="sb-vdo-info">
+                        <div class="sb-vdo-title"><a href="https://www.youtube.com/watch?v=pijivK5zfQA"><h4>Baap Baap Hota hai Beta Beta</h4></a></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+             <div class="sb-vdo-list">
+                    <div class="sb-vdo-thumb">
+                        <iframe width="250" height="170" src="http://www.youtube.com/embed/7VDN-yPr9as"></iframe></a>
+                    </div>
+                    <div class="sb-vdo-info">
+                        <div class="sb-vdo-title"><a href="https://www.youtube.com/watch?v=7VDN-yPr9as"><h4>Shoaib Akhtar Kill Pakistani Team</h4></a></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="more-but"><a href="index.php">View More >></a></div>
+                <div class="clear"></div>
+            </div>
+        </div>	
+        <div class="clear"></div>
+    </div>
+    <div class="footer">
+        <div class="clear"></div>
+        <div class="sub-footer">
+                <div class="copy"> © 2015 MaukaMaukaOfficial - All Rights Reserved</div>
+        </div>
+    </div>
+        <div class="clear"></div>
+    </div>
+</body>
+</html>
