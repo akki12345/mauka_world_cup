42e106c39cb783ff662b8636695992fe1f5a6328
akki12345 <akoli0082@gmail.com>
2015-03-16 16:59:53 +0530

    ab de

--- talk.php
+++ talk.php
@@ -63,11 +63,7 @@ require 'header.php';
 <ul class="list-group">
 <li class="list-group-item"><strong>Rating:</strong> <span class="media-label">9.8</span></li>
 </ul>
-<section class="container">
-<div class="row">
-<div class="col-md-12">
-<ol class="breadcrumb">
-<li><a href="index.php">Home</a></li>
+
 </div>
 </section>
 <br>
@@ -113,61 +109,6 @@ require 'header.php';
 </section>
 <br>
 
-<<<<<<< HEAD
-=======
-=======
-<!--  Start Left Section  col-md-9  -->
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
-<section class="container">
-<div class="row">
-<!--  Start Left Section  col-md-9  -->
-<div class="col-md-2 sidebar-secondary">
-<div class="thumbnail">
-
-<div class="ts-overlay ts-overlay-hover">
-<<<<<<< HEAD
-                                    <img src="./cup/ab.jpg" alt="Image">
-=======
-                                    <img src="./cup/sachin.jpg" alt="Image">
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
-                                    <div class="ts-overlay-panel ts-overlay-fade ts-overlay-background">
-                                        <p>Check our Memes</p>
-                                    </div>
-                                    <a class="ts-position-cover" href="#"></a>
-                                </div>
-
-
-
-</div>
-</div>
-<div class="col-md-7">
-<div class="main-content no-pad">
-<div class="col-md-12">
-<h3>Ask Any Query To Sachin <span class="pull-right">10</span></h3>
-<p class="lead text-muted">(2015)</p>
-</div>
-<div class="panel panel-default"><!-- Default panel contents -->
-<div class="panel-body">
-<<<<<<< HEAD
-<p>he is a former west indies player, widely acknowledged as the greatest batsman of all time. He took up cricket at the age of fifteen, made his Test debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen. He is the only player to have scored one hundred international centuries, the first batsman to score a double century in a One Day International, holds the record for most number of runs in both ODI and Test cricket, the only player to complete more than 30,000 runs in international cricket</p>
-<a data-toggle="modal" href="#talkchris" type="button" class="btn btn-primary">TALK</a>
-=======
-<p>Sachin is a former Indian cricketer, widely acknowledged as the greatest batsman of all time. He took up cricket at the age of eleven, made his Test debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen. He is the only player to have scored one hundred international centuries, the first batsman to score a double century in a One Day International, holds the record for most number of runs in both ODI and Test cricket, the only player to complete more than 30,000 runs in international cricket</p>
-<a data-toggle="modal" href="#talk" type="button" class="btn btn-primary">TALK</a>
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
-</div>
-<div class="panel-body">
-
-</div>
-<!-- List group -->
-<ul class="list-group">
-<li class="list-group-item"><strong>Rating:</strong> <span class="media-label">9.8</span></li>
-</ul>
-
-</div>
-</section>
-<br>
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
 
 <div class="col-md-2 sidebar-secondary">
 <div class="thumbnail">
@@ -346,15 +287,7 @@ require 'header.php';
   </div>
 </div>
 
-<<<<<<< HEAD
 <div class="modal fade" id="talkchris" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-=======
-<<<<<<< HEAD
-<div class="modal fade" id="talkchris" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-=======
-<div class="modal fade" id="talkronaldo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -402,19 +335,9 @@ require 'header.php';
   </div>
 </div>
 
-<<<<<<< HEAD
 
 
 <div class="modal fade" id="talkronaldo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-=======
-<<<<<<< HEAD
-
-
-<div class="modal fade" id="talkronaldo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-=======
-<div class="modal fade" id="talkdhoni" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -422,10 +345,6 @@ require 'header.php';
         <h4 class="modal-title" id="myModalLabel"><center>Talk</center></h4>
       </div>
       <div class="modal-body">
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
 
 
 <form class="form-horizontal" role="form" method="get" action = "#" onSubmit = "return validate(this)">
@@ -479,16 +398,6 @@ require 'header.php';
 <form class="form-horizontal" role="form" method="get" action = "#" onSubmit = "return validate(this)">
   
 
-<<<<<<< HEAD
-=======
-=======
-
-
-<form class="form-horizontal" role="form" method="get" action = "#" onSubmit = "return validate(this)">
-  
-
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
 
 
   <div class="form-group">
@@ -523,9 +432,6 @@ require 'header.php';
     </div>
   </div>
 </div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
 <div class="modal fade" id="talkkohli" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
  <div class="modal-dialog">
@@ -608,94 +514,6 @@ require 'header.php';
       </div>
       <div class="blog-footer">
         <p>MaukaMaukaOfficial © 2015</p>
-=======
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
-
-<div class="modal fade" id="talkkohli" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
- <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel"><center>Talk</center></h4>
-      </div>
-      <div class="modal-body">
-
-
-<form class="form-horizontal" role="form" method="get" action = "#" onSubmit = "return validate(this)">
-  
-
-
-
-  <div class="form-group">
-    
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
-  <textarea class="form-control" placeholder = "Your Query*(Min. 25 characters)" rows="5" id="inputMessage"></textarea>
-    </div>
-  </div>
-
-  
-  <div class="form-group">
-   <div class="col-md-1"></div>
-    <div class="col-md-10">
-      <a type="button" data-target="#success" data-toggle="modal" href="#" class="btn btn-primary" onsubmit="return checkForm(this);">Submit</a>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
-    <p>Field's marked with * are mandatory.</p>
-  </div>
-  </div>
-</form>
-
-      </div>
-      
-      <div class="blog-footer">
-     
-       <p>MaukaMaukaOfficial © 2015</p>        
-<<<<<<< HEAD
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-<!-- Modal -->
-<div class="modal fade" id="success" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Query Solved</h4>
-      </div>
-      <div class="modal-body">
-        <p><b><center><?php  
-  // connect to your MySQL database here 
-  require_once "config.php"; 
-  if(isset($_POST['submit']))  
-  // Build the sql command string
-  $submit = mysql_real_escape_string($_POST['submit']); 
-  // Execute the query here now 
-  $query = mysql_query("SELECT dialogs FROM ronaldo ORDER BY RAND() LIMIT 1") or die (mysql_error());  
-  // Output the data here using a while loop, the loop will return all members 
-  while ($row = mysql_fetch_array($query)) { 
-      // Gather all $row values into local variables for easier usage in output  
-        echo $row["dialogs"];  
-      // echo the output to browser or compound an output variables here 
-  } 
-  // close mysql connection 
-  mysql_close(); 
-  ?></center></p>
-  <a href="talk.php"><button type="button" data-toggle="modal"  class="btn btn-primary">Ask New Query</button></a>
-      </div>
-      <div class="blog-footer">
-        <p>MaukaMaukaOfficial © 2015</p>
-=======
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
       </div>
     </div>
   </div>
@@ -705,50 +523,6 @@ require 'header.php';
 
 <!--  ./ Left Section  col-md-9  -->
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<!-- Modal -->
-<div class="modal fade" id="success" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Query Solved</h4>
-      </div>
-      <div class="modal-body">
-        <p><b><center><?php  
-  // connect to your MySQL database here 
-  require_once "config.php"; 
-  if(isset($_POST['submit']))  
-  // Build the sql command string
-  $submit = mysql_real_escape_string($_POST['submit']); 
-  // Execute the query here now 
-  $query = mysql_query("SELECT dialogs FROM ronaldo ORDER BY RAND() LIMIT 1") or die (mysql_error());  
-  // Output the data here using a while loop, the loop will return all members 
-  while ($row = mysql_fetch_array($query)) { 
-      // Gather all $row values into local variables for easier usage in output  
-        echo $row["dialogs"];  
-      // echo the output to browser or compound an output variables here 
-  } 
-  // close mysql connection 
-  mysql_close(); 
-  ?></center></p>
-  <a href="talk.php"><button type="button" data-toggle="modal"  class="btn btn-primary">Ask New Query</button></a>
-      </div>
-      <div class="blog-footer">
-        <p>MaukaMaukaOfficial © 2015</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<!--  ./ Left Section  col-md-9  -->
-
->>>>>>> 19808a46f774973a8f21ef83bdd9cf7c78c8ca1d
->>>>>>> b91a974c868f1ffee612e43ed7c105128f06771c
 <!--  ./ row   --></section>
 
 <script>
@@ -773,4 +547,3 @@ return ""
 <?php
 require 'footer.php';
 ?>
-
